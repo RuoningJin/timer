9918fa03e3e14fa187c6a3c1b83d1af39a2ba4dc
RuoningJin <ronjin2013@hotmail.com>
2022-11-06 15:13:45 -0500

    added more complex timer2

---
+++ timer2.js
@@ -0,0 +1,20 @@
+const stdin = process.stdin;
+const stdout = process.stdout;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+stdin.on('data', (key) => {
+  if (key === '\u0062') {
+    stdout.write('\x07');
+  }
+  if (!isNaN(key)) {
+    console.log(`setting timer for ${key} seconds...`);
+    setTimeout(() => {
+      stdout.write('\x07');
+    }, key * 1000);
+  }
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!');
+    process.exit();
+  }
+});
